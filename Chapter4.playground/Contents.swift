a7af213bc459985a65817c7182bf2199e0297b77
Davide <16617843+monsud@users.noreply.github.com>
2019-10-22 17:41:19 +0200

    update
    update 4

--- Chapter4.playground/Contents.swift
+++ Chapter4.playground/Contents.swift
@@ -7,46 +7,34 @@ import PlaygroundSupport
     var haze: UIImage!
     var nest: UIImage!
 
-
     override func loadView() {
         let myView = UIView()
-        
-       
-        
+           
         let sfondoMainView = UIImageView()
         sfondoMainView.frame = CGRect(x: 0, y: 0, width: 1100, height: 700)
         sfondo = UIImage(named: "background.jpg")
         sfondoMainView.image = sfondo
         myView.addSubview(sfondoMainView)
-        
-    
-        
-        
+  
         //----> Nest
         
         let nestView = UIImageView()
-        nestView.frame = CGRect(x: 180, y: 550, width: 100, height: 80)
+        nestView.frame = CGRect(x: 180, y: 530, width: 100, height: 80)
         nest = UIImage(named: "nest.png")
         nestView.image = nest
         myView.addSubview(nestView)
-        
-        
-        
+ 
         // ----> Haze
         let hazeView = UIImageView()
-        hazeView.frame = CGRect(x: 220, y: 550, width: 100, height: 80)
-        haze = UIImage(named: "haze.png")
+        hazeView.frame = CGRect(x: 190, y: 486, width: 100, height: 80)
+        haze = UIImage(named: "hazesl.png")
         hazeView.image = haze
         myView.addSubview(hazeView)
         // ------
-        
-        
-    
+  
         self.view = myView
     }
 }
-
-
 
     let viewController = ViewController()
     PlaygroundPage.current.liveView = viewController
